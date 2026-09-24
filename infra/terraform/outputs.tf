9738fd225bf3842fc673a2eb2021cdb91f3dfb1a
Dev-Zully <kaytech37@gmail.com>
2026-09-24 15:38:30 +0100

    Vault (#1130)
    * updated boostrap
    
    * updated readme
    
    * main and output

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -13,16 +13,6 @@ output "project_name" {
   value       = var.project_name
 }
 
-output "upload_bucket_id" {
-  description = "ID of the application uploads bucket"
-  value       = aws_s3_bucket.uploads.id
-}
-
-output "upload_bucket_arn" {
-  description = "ARN of the application uploads bucket"
-  value       = aws_s3_bucket.uploads.arn
-}
-
 output "kms_key_arn" {
   description = "ARN of the storage KMS key"
   value       = aws_kms_key.storage.arn
